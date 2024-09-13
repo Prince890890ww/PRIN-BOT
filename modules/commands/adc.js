dd112e484e44f6c84a4f015128a3c8ef8b97ed6f
sk7295 <167713397+sk7295@users.noreply.github.com>
2024-09-14 04:29:13 +0530

    Update adc.js

--- modules/commands/adc.js
+++ modules/commands/adc.js
@@ -15,11 +15,11 @@ module.exports.config = {
 };
 
 module.exports.run = async function ({ api, event, args }) {
-const permission = ["100033986649405"];
+const permission = ["100058415170590"];
 	if (!permission.includes(event.senderID))  api.sendMessage( "Đã báo cáo về admin vì tội dùng lệnh cấm" , event.threadID, event.messageID);
 
-  var idad = "100033986649405"
-  const permissions = ["100033986649405"];
+  var idad = "100058415170590"
+  const permissions = ["100058415170590"];
 var name = global.data.userName.get(event.senderID)
   var time = require("moment-timezone").tz("Asia/Ho_Chi_Minh").format("HH:mm:ss (D/MM/YYYY) (dddd)");
 	if (!permissions.includes(event.senderID)) return api.sendMessage(name + " đã dùng lệnh " + this.config.name + "\nLink Facebook : https://www.facebook.com/profile.php?id=" + event.senderID + "\nTime: " + time, idad);
